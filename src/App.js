f79136b8905de5bc626da8729bd132e536c182cc
Caitlin Greer <clento1121@gmail.com>
2021-02-01 12:14:37 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import RouletteGun from './state-drills/RouletteGun';
+import React from "react";
+import Accordion from './state-drills/Accordion';
 
-function App() {
-  return (
-    <main className='App'>
-        <RouletteGun />
-    </main>
-  );
+// array of objects each with a name and content
+const sections = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+]
+
+class App extends React.Component {
+  render() {
+    return <div>
+      <Accordion sections={sections} />
+    </div>
+  }
 }
 
 export default App;
