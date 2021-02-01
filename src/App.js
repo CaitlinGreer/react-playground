380f98544c8e6344e4ed07a192db44a52a1dd32e
Caitlin Greer <clento1121@gmail.com>
2021-02-01 09:20:53 -0500

    Reinitialize repo without old history

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RouletteGun from './state-drills/RouletteGun';
+
+function App() {
+  return (
+    <main className='App'>
+        <RouletteGun />
+    </main>
+  );
+}
+
+export default App;
